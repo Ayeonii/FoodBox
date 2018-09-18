29b5b40a84733f766b6bcd48ccd00bddec772f89
LeeAYeon <dldkekdusz@naver.com>
2018-09-18 21:36:44 +0900

    한솔쓰 이거 pull 하면 돼

---
+++ app/src/main/java/com/example/dldke/foodbox/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dldke.foodbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("Hello SongHansol");
+        //이거 수정하구 너 코드나 자바 파일 추가할 거 있음 추가한다음 commit 누르고 push 하믄 댕
+    }
+}
