485b25b178712b75ba3b8953b91a9f07586cd96e
songhansol <dkwlrjf@ajou.ac.kr>
2018-10-20 22:22:31 +0900

    풀레시피 등록시 데이터베이스에 저장시키기 완료

--- app/src/main/java/com/example/dldke/foodbox/Activity/FullRecipeActivity.java
+++ app/src/main/java/com/example/dldke/foodbox/Activity/FullRecipeActivity.java
@@ -1,30 +1,36 @@
 package com.example.dldke.foodbox.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Spinner;
 
 import com.example.dldke.foodbox.Adapter.FullRecipeAdapter;
+import com.example.dldke.foodbox.Adapter.FullRecipeHorizontalAdapter;
+import com.example.dldke.foodbox.DataBaseFiles.Mapper;
+import com.example.dldke.foodbox.DataBaseFiles.RecipeDO;
 import com.example.dldke.foodbox.FullRecipeDictionary;
+import com.example.dldke.foodbox.FullRecipeHorizontalData;
 import com.example.dldke.foodbox.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class FullRecipeActivity extends AppCompatActivity  {
 
     private ArrayList<FullRecipeDictionary> mArrayList;
     private FullRecipeAdapter mAdapter;
     private RecyclerView mRecyclerView, mHorizontalView;
-    private HorizontalAdapter hAdapter;
+    private FullRecipeHorizontalAdapter hAdapter;
     private LinearLayoutManager mLayoutManager;
 
     private int MAX_ITEM_COUNT=10;
@@ -36,7 +42,10 @@ public class FullRecipeActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_recipe);
 
-        //DB에 사용자 풀레시피 만들기
+        //DB에 풀레시피 만들기
+        final List<RecipeDO.Ingredient> specIngredientList = new ArrayList<>();
+        final List<RecipeDO.Spec> specList = new ArrayList<>();
+        final String recipe_id;
 
         mRecyclerView = (RecyclerView)findViewById(R.id.recyclerview_main_list);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -45,10 +54,41 @@ public class FullRecipeActivity extends AppCompatActivity  {
         mAdapter = new FullRecipeAdapter(this, mArrayList);
         mRecyclerView.setAdapter(mAdapter);
 
+        final EditText foodtitle_et = (EditText)findViewById(R.id.food_title);
+
+        /*
+        요리 카테고리 만드는 spinner
+         */
+        Spinner spinner = (Spinner)findViewById(R.id.food_spinner);
+        String[] foodrecipe_list = new String[]{"메인요리","국/찌개","반찬","간식"};
+
+        ArrayAdapter<String> spinnerAdapter;
+        spinnerAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, foodrecipe_list);
+        spinner.setAdapter(spinnerAdapter);
+
+        /*
+        커뮤니티 객체 만들기
+         */
+        List<String> recipe_list = Mapper.searchMyCommunity().getMyRecipes();
+        recipe_id = recipe_list.get(0);
+        Log.d(TAG, "뭔지 모르겠는 데이터!!!!" + Mapper.searchRecipe(recipe_id).getIngredient().get(0).getIngredientName());
+
+
+        /*
+        풀레시피에 간이레시피에 등록된 식재료 입력
+         */
+        specIngredientList.add(Mapper.searchRecipe(recipe_id).getIngredient().get(0));
+        specIngredientList.add(Mapper.searchRecipe(recipe_id).getIngredient().get(1));
+
+        /*
+        풀레시피 detail 작성 팝업창
+         */
         Button ingredient_btn = (Button)findViewById(R.id.button_main_insert);
         ingredient_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //팝업창 build
                 AlertDialog.Builder builder = new AlertDialog.Builder(FullRecipeActivity.this);
                 View view = LayoutInflater.from(FullRecipeActivity.this)
                         .inflate(R.layout.fullrecipe_edit_box, null, false);
@@ -66,13 +106,20 @@ public class FullRecipeActivity extends AppCompatActivity  {
                     public void onClick(View v) {
                         String strID = editTextID.getText().toString();
                         String strEnglish = editTextEnglish.getText().toString();
+                        Integer intEnglish = Integer.parseInt(strEnglish);
                         String strKorean = editTextKorean.getText().toString();
 
                         FullRecipeDictionary dict = new FullRecipeDictionary(strID, strEnglish, strKorean );
 
-                        mArrayList.add(0, dict); //첫 줄에 삽입
-                        //mArrayList.add(dict); //마지막 줄에 삽입
+                        //mArrayList.add(0, dict); //첫 줄에 삽입
+                        mArrayList.add(dict); //마지막 줄에 삽입
                         mAdapter.notifyDataSetChanged(); //변경된 데이터를 화면에 반영
+
+
+                        //풀레시피에 단계별 레시피 등록
+                        RecipeDO.Spec spec = Mapper.createSpec(specIngredientList, strID, strKorean,intEnglish);
+                        specList.add(spec);
+                        Log.d(TAG, "방법 : "+strID + "불 세기 : "+strKorean +"시간 : "+intEnglish);
 
                         dialog.dismiss();
                     }
@@ -82,6 +129,36 @@ public class FullRecipeActivity extends AppCompatActivity  {
             }
         });
 
+        /*
+        작성 완료시 풀레시피 데이터 DB에 저장하기
+         */
+        Button ok_btn = (Button)findViewById(R.id.recipe_ok);
+        ok_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String FoodTitle = foodtitle_et.getText().toString();
+                Mapper.createFullRecipe(recipe_id, FoodTitle, specList);
+                Intent RefrigeratorActivity = new Intent(getApplicationContext(), RefrigeratorMainActivity.class);
+                startActivity(RefrigeratorActivity);
+            }
+        });
+
+        /*
+        레시피 재료 선택하기
+         */
+        Button Ingredient_add = (Button)findViewById(R.id.ingredient_add);
+        Ingredient_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent choice_ingred = new Intent(getApplicationContext(), PencilRecipeActivity.class);
+                startActivity(choice_ingred);
+            }
+        });
+
+
+        /*
+        선택된 재료 보여주기
+         */
         mHorizontalView = (RecyclerView)findViewById(R.id.recyclerview_horizontal_list);
 
         mLayoutManager = new LinearLayoutManager(this);
@@ -89,13 +166,13 @@ public class FullRecipeActivity extends AppCompatActivity  {
 
         mHorizontalView.setLayoutManager(mLayoutManager);
 
-        hAdapter = new HorizontalAdapter();
+        hAdapter = new FullRecipeHorizontalAdapter();
 
-        ArrayList<HorizontalData> data = new ArrayList<>();
+        ArrayList<FullRecipeHorizontalData> data = new ArrayList<>();
 
         int i = 0;
         while(i<MAX_ITEM_COUNT){
-            data.add(new HorizontalData(R.drawable.food, i+"번째 데이터"));
+            data.add(new FullRecipeHorizontalData(R.drawable.food, i+"번째 데이터"));
             i++;
         }
 
@@ -104,70 +181,4 @@ public class FullRecipeActivity extends AppCompatActivity  {
 
     }
 }
-class HorizontalAdapter extends RecyclerView.Adapter<HorizontalViewHolder>{
 
-    private ArrayList<HorizontalData> HorizontalDatas;
-
-    public void setData(ArrayList<HorizontalData> list){
-        HorizontalDatas = list;
-    }
-
-    @Override
-    public HorizontalViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        // 사용할 아이템의 뷰를 생성해준다.
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fullrecipe_ingredient_list, parent, false);
-
-        HorizontalViewHolder holder = new HorizontalViewHolder(view);
-
-        return holder;
-    }
-
-    @Override
-    public void onBindViewHolder(HorizontalViewHolder holder, int position) {
-        HorizontalData data = HorizontalDatas.get(position);
-
-        holder.description.setText(data.getText());
-        holder.icon.setImageResource(data.getImg());
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return HorizontalDatas.size();
-    }
-}
-
-class HorizontalViewHolder extends RecyclerView.ViewHolder {
-
-    public ImageView icon;
-    public TextView description;
-
-    public HorizontalViewHolder(View itemView) {
-        super(itemView);
-
-        icon = (ImageView) itemView.findViewById(R.id.horizon_icon);
-        description = (TextView) itemView.findViewById(R.id.horizon_description);
-
-    }
-}
-
-class HorizontalData {
-
-    private int img;
-    private String text;
-
-    public HorizontalData(int img, String text){
-        this.img = img;
-        this.text = text;
-    }
-
-    public String getText() {
-        return this.text;
-    }
-
-    public int getImg() {
-        return this.img;
-    }
-}
